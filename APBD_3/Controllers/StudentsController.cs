d896ebcf1d02ef684682b2e51079342a7e6d129f
KrzysztofMaj-s18533 <s18533@pjwstk.edu.pl>
2020-03-18 22:18:31 +0100

    Commiting full code for exercise

---
+++ APBD_3/Controllers/StudentsController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using APBD_3.DAL;
+using APBD_3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APBD_3.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        private readonly IDbService _dbService;
+
+        public StudentsController(IDbService dbService)
+        {
+            _dbService = dbService;
+        }
+
+        //[HttpGet("{id}")]
+        //public IActionResult GetStudents([FromRoute] int id)
+        //{
+        //    if (id == 1) {
+        //        return Ok("Nowak");
+        //    }
+        //    else if(id == 2)
+        //    {
+        //        return Ok("Kowalski");
+        //    }
+        //    return NotFound("Nie znaleziono studenta o id '" + id +"'");
+        //}
+
+        [HttpGet]
+        public IActionResult GetStudents(string orderBy)
+        {
+            return Ok(_dbService.GetStudents());
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudent (Student student)
+        {
+            student.indexNumber = $"s{new Random().Next(1, 20000)}";
+            return Ok(student);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateStudent([FromRoute] int id)
+        {
+            return Ok("Aktualizacja dokończona");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult RemoveStudent([FromRoute] int id)
+        {
+            return Ok("Usuwanie ukończone");
+        }
+    }
+}
---
+++ APBD_3/DAL/MockDbService.cs
@@ -0,0 +1,27 @@
+﻿using APBD_3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APBD_3.DAL
+{
+    public class MockDbService : IDbService
+    {
+        private static IEnumerable<Student> _students;
+
+        static MockDbService()
+        {
+            _students = new List<Student>
+            {
+                new Student{idStudent=1, firstName="Maciej", lastName="Kowalski" },
+                new Student{idStudent=2, firstName="Anna", lastName="Rzeczna" },
+                new Student{idStudent=1, firstName="Piotr", lastName="Siódmiak" }
+            };
+        }
+        public IEnumerable<Student> GetStudents()
+        {
+            return _students;
+        }
+    }
+}
